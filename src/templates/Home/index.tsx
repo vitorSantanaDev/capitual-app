31b6ba5af758ddcd71246bab05d334688a603790
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-11-26 18:51:53 -0300

    feat: :art: creating HomeTemplate

---
+++ src/templates/Home/index.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import * as M from '@mui/material'
+import { FooterComponent } from '../../components'
+
+const HomeTemplate: React.FC = () => {
+	return (
+		<M.Container maxWidth="lg">
+			<FooterComponent />
+		</M.Container>
+	)
+}
+
+export default HomeTemplate
