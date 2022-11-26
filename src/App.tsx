4f987b62b0954016e452ec31fae8da5a14445c86
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-11-25 21:57:00 -0300

    chore: :tada: initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react'
+import * as M from '@mui/material'
+import * as S from './styled'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App: React.FC = () => {
+	return (
+		<S.AppWrapper>
+			<M.Button className="button" variant="contained">
+				Hello World
+			</M.Button>
+		</S.AppWrapper>
+	)
 }
 
-export default App;
+export default App
